6484e2010194867ca10b4f8dd021a42f2d3b6391
lqb1996 <lqb1139134758@163.com>
2020-03-03 17:57:50 +0800

    cosine_similarity_linking

--- map_decoder/show2label.py
+++ map_decoder/show2label.py
@@ -1,26 +1,12 @@
 import os
-import cv2
-import json
+import json, time, datetime
 
-input_imgs = '../../scrapy_structure/military/USNI_images/'
+input_imgs = '../../lqb_gis/USNI_images/'
 files = os.listdir(input_imgs)
-input_json = './location.json'
-output_json = './labeled_location.json'
 
-with open(input_json, 'r')as c_f:
-    s = c_f.readline()
-    obj_j = json.loads(s)
-    for o in obj_j:
-        print(o['time'])
-    # for f in files:
-    #     template = cv2.imread(input_path, 0)
-    #     h, w = template.shape[:2]
-    #     font = cv2.FONT_HERSHEY_SIMPLEX  # 定义字体
-    #     for r in de_res:
-    #         cv2.circle(img_rgb, (int(r['location'][1]), int(r['location'][0])), 40, (0, 0, 255), 4)
-    #         imgzi = cv2.putText(img_rgb, r['words'], (int(r['location'][1] + w), int(r['location'][0] + h)), font, 1.2, (255, 255, 255), 2)
-    #         # 图像，文字内容， 坐标 ，字体，大小，颜色，字体厚度
-    #     cv2.namedWindow('img_rgb', cv2.WINDOW_NORMAL)
-    #     cv2.imshow('img_rgb', img_blue)
-    #     cv2.waitKey(0)
-    #     cv2.destroyAllWindows()
+for f in files:
+    print("old name is:", os.path.join(input_imgs, f))
+    t = time.strptime(f, "%B %d, %Y %I:%M %p")
+    formated_name = time.strftime("%Y-%m-%d %H:%M:%S", t)
+    print("new name is:", os.path.join(input_imgs, formated_name))
+    os.rename(os.path.join(input_imgs, f), os.path.join(input_imgs, formated_name))
