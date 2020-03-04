2cd2f115c615d0456081f7aa94b9dfcf1e5d1c6d
lqb1996 <lqb1139134758@163.com>
2020-03-04 17:25:06 +0800

    cosine_similarity_linking

--- map_decoder/get_reagon.py
+++ map_decoder/get_reagon.py
@@ -1,17 +1,30 @@
-import os, json
+import os, json, csv
 import sklearn
 
 
-reagon_f = './location.json'
+labeled_f = './labeled_data.json'
+entity_f = './named_entity.csv'
 res_list = []
+uncognized = []
 
-with open(reagon_f) as f:
-    reagon_json = f.readline()
-    o_json = json.loads(reagon_json)
-    for o in o_json:
-        for p in o['points_list']:
-            low = p['words'].lower()
-            if 'reagan' in low or 'ronald' in low:
-                p.update({'time': o['time']})
-                res_list.append(p)
+with open(labeled_f) as f:
+    labeled_str = ''
+    for l in f.readlines():
+        labeled_str += l
+    o_json = json.loads(labeled_str)
+    with open(entity_f) as e_f:
+        e_csv = csv.reader(e_f)
+        e_list = []
+        for i, row in enumerate(e_csv):
+            if i is not 0:
+                e_list.append([row[0], row[1].lower().split(' '), row[2]])
+                res_list.append([])
+        for o in o_json:
+            for p in o['points_list']:
+                low = p['words'].lower()
+                for e in e_list:
+                    for w in e[1]:
+                        if w in low:
+                            p.update({'time': o['time']})
+                            res_list[int(e[0])].append(p)
 print(res_list)
